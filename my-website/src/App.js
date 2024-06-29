96f54913cc6c1b14b9ff221161dc6720c055b8a9
gzuzson <mail@jeremiahsaemo.com>
2024-06-29 13:34:18 +1000

    Added dark mode functionality within website: - defines css variables within index.css - components across app refer to variables defined - added toggle button component to switch from light to dark mode

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Hero from "./components/Hero";
+import Hero from "./components/Hero/Hero";
+import React, {useState} from "react";
+import {Toggle} from "./components/Toggle/Toggle";
+import useLocalStorage from "use-local-storage";
 
 function App() {
+    const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
   return (
-    <div className="App">
+    <div className="App" data-theme={isDark ? "dark" : "light"}>
       <header className="App-header">
-        <Hero></Hero>
+          <Toggle
+            isChecked={isDark}
+            handleChange={() => setIsDark(!isDark)}
+          />
+          <Hero />
       </header>
     </div>
   );
---
+++ my-website/src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+    return (
+      <h1 className={styles.Hero}>Hello, World</h1>
+    );
+}
+
+export default Hero;
